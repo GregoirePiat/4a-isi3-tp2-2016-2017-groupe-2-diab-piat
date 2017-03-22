2017f74baab7eca202417308282103e0b5062f8d
GregoirePiat <gregoire.piat@outlook.fr>
2017-03-22 11:30:36 +0100

    New methods on VisiterPostfixe

--- src/main/java/VisiteurPostfixe.java
+++ src/main/java/VisiteurPostfixe.java
@@ -2,29 +2,29 @@ public class VisiteurPostfixe implements Visiteur {
 
     public void visiterAddition(Addition addition) {
 
-        addition.getOperateurGauche().accept(this);
-        addition.getOperateurDroit().accept(this);
-        addition.getOperateur();
-
     }
 
     public void visiterMultiplication(Multiplication multiplication) {
-
+        multiplication.getOperateurGauche().accept(this);
+        multiplication.getOperateurDroit().accept(this);
+        System.out.print(multiplication.getOperateur());
     }
 
     public void visiterNegation(Negation negation) {
 
     }
 
-    public void visiterConstante(Constante constante) {
-
-    }
-
     public void visiterUnaire(OperateurUnaire operateurUnaire) {
 
+        operateurUnaire.getOperateurGauche().accept(this);
+        System.out.print(operateurUnaire.getOperateur());
     }
 
     public void visiterBinaire(OperateurBinaire operateurBinaire) {
+
+        operateurBinaire.getOperateurGauche().accept(this);
+        operateurBinaire.getOperateurDroit().accept(this);
+        System.out.print(operateurBinaire.getOperateur());
 
     }
 }
