9a5d36ef9cba780450cdda07181ff4f8673fe637
NicolasDiab <nicolas-diab@hotmail.fr>
2017-03-26 11:40:27 +0200

    VisiteurCalculerHauteur marche bien !

--- src/main/java/VisiteurCalculerHauteur.java
+++ src/main/java/VisiteurCalculerHauteur.java
@@ -2,26 +2,34 @@ import java.util.OptionalInt;
 
 public class VisiteurCalculerHauteur implements Visiteur {
     public OptionalInt visiterAddition(Addition addition) {
-        return OptionalInt.empty();
+        return this.visiterBinaire(addition);
     }
 
     public OptionalInt visiterMultiplication(Multiplication multiplication) {
-        return OptionalInt.empty();
+        return this.visiterBinaire(multiplication);
     }
 
     public OptionalInt visiterNegation(Negation negation) {
-        return OptionalInt.empty();
+        return this.visiterUnaire(negation);
     }
 
     public OptionalInt visiterConstante(Constante constante) {
-        return OptionalInt.empty();
+        return OptionalInt.of(1);
     }
 
     public OptionalInt visiterUnaire(OperateurUnaire operateurUnaire) {
+        OptionalInt hauteur = operateurUnaire.getOperateurGauche().accept(this);
+        if (hauteur.isPresent())
+            return OptionalInt.of(hauteur.getAsInt() + 1);
         return OptionalInt.empty();
     }
 
     public OptionalInt visiterBinaire(OperateurBinaire operateurBinaire) {
+        OptionalInt hauteurGauche = operateurBinaire.getOperateurGauche().accept(this);
+        OptionalInt hauteurDroit = operateurBinaire.getOperateurDroit().accept(this);
+
+        if (hauteurGauche.isPresent() && hauteurDroit.isPresent())
+            return OptionalInt.of(Math.max(hauteurGauche.getAsInt(), hauteurDroit.getAsInt()) + 1);
         return OptionalInt.empty();
     }
 }
